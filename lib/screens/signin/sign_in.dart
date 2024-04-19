6c6b7c93dc7e269cfdcd5e6af212f92e786b5ea5
DrawTok <lth121103@gmail.com>
2024-04-19 23:57:02 +0700

    sign in screen

--- lib/screens/signin/sign_in.dart
+++ lib/screens/signin/sign_in.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:fruitshop/screens/home/widgets/app_bar.dart';
+import 'package:fruitshop/widgets/app_bar.dart';
 import 'package:fruitshop/screens/signin/widgets/form.dart';
 import 'package:fruitshop/utils/constants/sizes.dart';
 import 'package:fruitshop/utils/constants/text_strings.dart';
+import 'package:fruitshop/utils/constants/styles.dart';
 
 class SignIn extends StatelessWidget {
   const SignIn({super.key});
@@ -14,7 +15,7 @@ class SignIn extends StatelessWidget {
         appBar: const TAppBar(title: TTexts.signInName),
         body: SingleChildScrollView(
           child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: TSizes.spacing25),
+            padding: const EdgeInsets.symmetric(horizontal: TSizes.spacing24),
             child: Column(
               children: [
                 Image.asset(
@@ -22,7 +23,7 @@ class SignIn extends StatelessWidget {
                   height: 200,
                 ),
                 const SizedBox(height: TSizes.sizeBoxHeight20),
-                Text('Chào mừng trở lại!'),
+                Text(TTexts.welcomeBack.toUpperCase(), style: Styles.heading1),
                 const SizedBox(height: TSizes.sizeBoxHeight20),
                 const SignInForm(),
                 const SizedBox(height: TSizes.sizeBoxHeight50),
--- lib/screens/signin/widgets/form.dart
+++ lib/screens/signin/widgets/form.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:fruitshop/controllers/signin/signin_controller.dart';
 import 'package:fruitshop/utils/constants/colors.dart';
-import 'package:fruitshop/utils/constants/constant.dart';
 import 'package:fruitshop/utils/constants/sizes.dart';
+import 'package:fruitshop/utils/constants/styles.dart';
 import 'package:fruitshop/utils/constants/text_strings.dart';
+import 'package:fruitshop/widgets/custom_button.dart';
 import 'package:fruitshop/widgets/divider.dart';
 import 'package:get/get.dart';
 
@@ -17,66 +18,36 @@ class SignInForm extends GetView<SignInController> {
       autovalidateMode: AutovalidateMode.onUserInteraction,
       child: Column(
         children: [
-          const Padding(
-            padding: EdgeInsets.symmetric(
-                vertical: TSizes.spacing8, horizontal: TSizes.spacing10),
-            child: Row(
-              children: [Text('Email '), textRequired],
-            ),
-          ),
           TextFormField(
             onChanged: controller.setEmail,
             validator: (value) => controller.validateEmail(value),
-            decoration: _buildInputDecoration('Nhập email của bạn'),
+            decoration: Styles.buildInputDecoration('Nhập email của bạn'),
           ),
-          const SizedBox(height: TSizes.spaceBtwInputField),
-          const Padding(
-            padding: EdgeInsets.symmetric(
-                vertical: TSizes.spacing8, horizontal: TSizes.spacing10),
-            child: Row(
-              children: [Text('Mật khẩu '), textRequired],
-            ),
-          ),
+          const SizedBox(height: TSizes.spacing16),
           Obx(
             () => TextFormField(
               onChanged: controller.setPassword,
               obscureText: controller.showPassword.value,
               validator: (value) => controller.validatePassword(value),
-              decoration: _buildInputDecoration('Nhập mật khẩu',
+              decoration: Styles.buildInputDecoration('Nhập mật khẩu',
                   onTap: controller.visibilityPassword,
                   isShow: controller.showPassword.value),
             ),
           ),
-          const SizedBox(height: TSizes.sizeBoxHeight10),
+          const SizedBox(height: TSizes.spacing16),
           Align(
             alignment: Alignment.topRight,
             child: InkWell(
               onTap: () {},
               child: Text(
                 TTexts.forgotPassword,
-                style: TextStyle(color: TColors.greenPrimary),
+                style: TextStyle(color: TColors.grayPrimary),
               ),
             ),
           ),
-          const SizedBox(height: TSizes.sizeBoxHeight10),
-          SizedBox(
-            height: 50,
-            child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: TColors.greenPrimary,
-                    foregroundColor: Colors.white),
-                child: const Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(TTexts.signInName),
-                  ],
-                ),
-                onPressed: () => controller.onSubmit()),
-          ),
-          const SizedBox(height: TSizes.spacing20),
-          const SizedBox(height: TSizes.spacing20),
-          const FormDivider(text: TTexts.questionNoAccount),
-          const SizedBox(height: 30),
+          const SizedBox(height: TSizes.spacing16),
+          CustomButton(text: TTexts.signInName, onPressed: controller.onSubmit),
+          const SizedBox(height: TSizes.spacing32),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
@@ -85,35 +56,39 @@ class SignInForm extends GetView<SignInController> {
               Image.asset('assets/icons/twitter.png')
             ],
           ),
-          const SizedBox(height: 50),
+          const SizedBox(height: TSizes.spacing32),
+          const FormDivider(
+            text: TTexts.questionNoAccount,
+            actionText: TTexts.createAccount,
+          ),
         ],
       ),
     );
   }
 
-  InputDecoration _buildInputDecoration(String labelText,
+  /* InputDecoration _buildInputDecoration(String labelText,
       {void Function()? onTap, bool? isShow}) {
     return InputDecoration(
       contentPadding: const EdgeInsets.symmetric(
-          vertical: TSizes.spacing10, horizontal: TSizes.spacing20),
+          vertical: TSizes.spacing16, horizontal: TSizes.spacing20),
       fillColor: Colors.white,
       filled: true,
       focusedBorder: OutlineInputBorder(
         borderSide: BorderSide(color: TColors.greenPrimary, width: 0.5),
-        borderRadius: BorderRadius.circular(TSizes.borderRadius999),
+        borderRadius: BorderRadius.circular(TSizes.borderRadius10),
       ),
       enabledBorder: OutlineInputBorder(
         borderSide: BorderSide(color: TColors.graySecondary),
-        borderRadius: BorderRadius.circular(TSizes.borderRadius999),
+        borderRadius: BorderRadius.circular(TSizes.borderRadius10),
       ),
       hintText: labelText,
       errorBorder: OutlineInputBorder(
         borderSide: BorderSide(color: Colors.red.shade800),
-        borderRadius: BorderRadius.circular(TSizes.borderRadius999),
+        borderRadius: BorderRadius.circular(TSizes.borderRadius10),
       ),
       focusedErrorBorder: OutlineInputBorder(
         borderSide: BorderSide(color: Colors.red.shade800),
-        borderRadius: BorderRadius.circular(TSizes.borderRadius999),
+        borderRadius: BorderRadius.circular(TSizes.borderRadius10),
       ),
       /* errorStyle: Styles.styleErrorText,
       hintStyle: Styles.styleText14, */
@@ -126,5 +101,5 @@ class SignInForm extends GetView<SignInController> {
                   : Icons.remove_red_eye_outlined))
           : null,
     );
-  }
+  } */
 }
