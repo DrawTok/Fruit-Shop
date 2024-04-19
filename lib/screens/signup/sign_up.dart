99f2ac9419ce96dd4b1ceb570b264e1ad2ec6261
DrawTok <lth121103@gmail.com>
2024-04-19 23:57:31 +0700

    sign up screen

---
+++ lib/screens/signup/sign_up.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:fruitshop/widgets/app_bar.dart';
+import 'package:fruitshop/screens/signup/widgets/form.dart';
+import 'package:fruitshop/utils/constants/sizes.dart';
+import 'package:fruitshop/utils/constants/text_strings.dart';
+import 'package:fruitshop/utils/constants/styles.dart';
+
+class SignUp extends StatelessWidget {
+  const SignUp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: const TAppBar(title: TTexts.signUpName),
+        body: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.symmetric(horizontal: TSizes.spacing24),
+            child: Column(
+              children: [
+                Image.asset(
+                  'assets/icons/fruit_yeah.png',
+                  height: 200,
+                ),
+                const SizedBox(height: TSizes.sizeBoxHeight20),
+                Text(TTexts.welcomeBack.toUpperCase(), style: Styles.heading1),
+                const SizedBox(height: TSizes.sizeBoxHeight20),
+                const SignUpForm(),
+                const SizedBox(height: TSizes.sizeBoxHeight50),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/signup/widgets/form.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:fruitshop/controllers/signup/signup_controller.dart';
+import 'package:fruitshop/utils/constants/sizes.dart';
+import 'package:fruitshop/utils/constants/styles.dart';
+import 'package:fruitshop/utils/constants/text_strings.dart';
+import 'package:fruitshop/widgets/custom_button.dart';
+import 'package:fruitshop/widgets/divider.dart';
+import 'package:get/get.dart';
+
+class SignUpForm extends GetView<SignUpController> {
+  const SignUpForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: controller.formKeySignUp,
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      child: Column(
+        children: [
+          TextFormField(
+            onChanged: controller.setEmail,
+            validator: (value) => controller.validateEmail(value),
+            decoration: Styles.buildInputDecoration('Nhập email của bạn'),
+          ),
+          const SizedBox(height: TSizes.spacing16),
+          Obx(
+            () => TextFormField(
+              onChanged: controller.setPassword,
+              obscureText: controller.showPassword.value,
+              validator: (value) => controller.validatePassword(value),
+              decoration: Styles.buildInputDecoration('Nhập mật khẩu',
+                  onTap: controller.visibilityPassword,
+                  isShow: controller.showPassword.value),
+            ),
+          ),
+          const SizedBox(height: TSizes.spacing16),
+          Obx(
+            () => TextFormField(
+              onChanged: controller.setConfirmPassword,
+              obscureText: controller.showConfirmPassword.value,
+              validator: (value) => controller.comparePassword(value),
+              decoration:Styles.buildInputDecoration('Nhập lại mật khẩu',
+                  onTap: controller.visibilityConfirmPassword,
+                  isShow: controller.showConfirmPassword.value),
+            ),
+          ),
+          const SizedBox(height: TSizes.spacing16),
+          CustomButton(text: TTexts.signUpName, onPressed: controller.onSubmit),
+          const SizedBox(height: TSizes.spacing32),
+          const FormDivider(
+            text: TTexts.questionHaveAccount,
+            actionText: TTexts.signInName,
+          ),
+        ],
+      ),
+    );
+  }
+}
